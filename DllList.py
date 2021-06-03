23a4163bf0e03982d10ef846ae77cedd4f10985a
Personal-Identifier <38745332+Personal-Identifier@users.noreply.github.com>
2021-06-02 20:00:25 -0700

    Updated pslist version

--- DllList.py
+++ DllList.py
@@ -22,7 +22,7 @@ def get_requirements(cls):
                                     description = "Windows kernel symbols"),
             requirements.PluginRequirement(name = 'pslist',
                                plugin = pslist.PsList,
-                               version = (1, 0, 0)), 
+                               version = (2, 0, 0)), 
             requirements.ListRequirement(name = 'pid',
                              description = 'Filter on specific process IDs',
                              element_type = int,
