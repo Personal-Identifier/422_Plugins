d5ea6fd668d3c82c8cc2c43e5ccc3bdf095b8791
gabriel-sohn <gabriel-sohn1999@live.com>
2021-06-04 20:43:23 -0700

    pslistcodehelper

--- Linux 1/1pslist.py
+++ Linux 1/1pslist.py
@@ -1,4 +1,14 @@
+#Gabriel Sohn, Ellias Miller
 import os
 import subprocess
-os.system("echo Hello")
-subprocess.call("./vol.py -f linux-sample-1.bin linux.pslist", shell=True)
+exit = ""
+FILE = input("Enter file address: ")
+subprocess.call("./vol.py -f " + FILE + " linux.pslist", shell=True)
+while exit != 'quit':
+
+    PID = input("Enter Process or name or quit: ")
+    exit = PID
+    if exit != 'quit' and exit != '' and exit != None:
+        subprocess.call("./vol.py -f " + FILE + " linux.pslist | grep " + PID, shell=True)
+        print("Malfind search: ")
+        subprocess.call("./vol.py -f " + FILE + " linux.malfind | grep " + PID, shell=True)
